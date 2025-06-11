5120cd765b05b11a8c2e16e57c40a27b025dee2b
yixianghong <a1001034@gmail.com>
2025-06-11 23:07:36 +0800

    fix tailwindcss setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,35 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from "node:url";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-12-25",
   devtools: { enabled: true },
-
+  srcDir: "src/client/",
+  serverDir: "src/server",
+  dir: { public: "../../public" },
+  alias: {
+    "~types": fileURLToPath(new URL("./types", import.meta.url)),
+    "~server": fileURLToPath(new URL("./src/server", import.meta.url)),
+  },
+  vite: {
+    ssr: {
+      noExternal: ["104components"],
+    },
+    plugins: [tailwindcss()],
+  },
+  components: [{ path: "~/components" }],
+  imports: { dirs: ["composables/**"] },
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/scripts",
+  ],
+  css: ["~/assets/css/main.css"],
+  image: {
+    dir: "assets/imgs",
+  },
+});
---
+++ src/client/pages/AbcPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3ace01 mt-4">Test</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
