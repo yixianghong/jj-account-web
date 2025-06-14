c599946bc1741613e1745e5ce6dca035083ef1a9
Jason Hong[洪逸翔] <a10010134@gmail.com>
2025-06-14 13:02:52 +0800

    新增部署功能

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 建置專案
+npm run generate
+
+# 部署到 Firebase
+firebase deploy --only hosting 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,27 +6,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src/client/",
   serverDir: "src/server",
-  dir: { public: "../../public" },
   alias: {
     "~types": fileURLToPath(new URL("./types", import.meta.url)),
     "~server": fileURLToPath(new URL("./src/server", import.meta.url)),
   },
   vite: {
-    ssr: {
-      noExternal: ["104components"],
-    },
     plugins: [tailwindcss()],
   },
   components: [{ path: "~/components" }],
   imports: { dirs: ["composables/**"] },
   modules: [
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
+    "@nuxt/ui",
     "@nuxt/icon",
     "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/ui",
   ],
   css: ["~/assets/css/main.css"],
   image: {
@@ -49,5 +41,8 @@ export default defineNuxtConfig({
     }
   },
   ui: {
+  },
+  nitro: {
+    preset: 'static'
   }
 });
